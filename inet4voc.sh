54953797d4b08bf17065d5db427fb6a04e22d39b
Kenji Ogura <k5i.ogura.v40@gmail.com>
2020-04-28 15:38:02 +0900

    revise

--- inet4voc.sh
+++ inet4voc.sh
@@ -31,7 +31,7 @@ if [ ! -e inet-voc.list ];then
     grep -f voc.names words.inet.txt | awk '{print $1;}' > inet-voc.list
 fi
 
-./downloadutils.py --cat -n 900 --wnid_list inet-voc.list -th 20
+./downloadutils.py --cat -n 900 --wnid_list inet-voc.list -th 1
 
 echo geterate shortnames
 python make_shortnames.py -w words.txt -l imagenet.labels.safedomain900.list > imagenet.shortnames.safedomain900.list
