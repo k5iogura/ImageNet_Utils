217a7b1f92e348fffa7cdbf33af96ef428fd11df
Kenji Ogura <k5i.ogura.v40@gmail.com>
2020-05-12 04:43:26 +0000

    revise

--- make_trainval.sh
+++ make_trainval.sh
@@ -6,10 +6,10 @@ export valids=5000
 export trains=$(($jpgs-$valids))
 echo "train=" $trains "valid=" $valids
 
-find inet_images -iname \*.jpg | sort -R | head -$trains > train.txt
+find $(pwd)/inet_images -iname \*.jpg | sort -R | head -$trains > train.txt
 echo -n "check train jpeg= "; wc -l train.txt
 
-find inet_images -iname \*.jpg | sort -R | tail -$valids > valid.txt
+find $(pwd)/inet_images -iname \*.jpg | sort -R | tail -$valids > valid.txt
 echo -n "check valid jpeg= "; wc -l valid.txt
 
 echo Ready train.txt valid.txt
